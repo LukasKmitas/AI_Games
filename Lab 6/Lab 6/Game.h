e1bf650d6dc8c522c4b469f2d63514b58a3b4091
Lukas Kmitas <lukaskmitas@hotmail.co.uk>
2023-10-20 09:21:57 +0100

    Lab 6 starter

---
+++ Lab 6/Lab 6/Game.h
@@ -0,0 +1,34 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#include <SFML/Graphics.hpp>
+
+class Game
+{
+public:
+	Game();
+	~Game();
+
+	void run();
+
+private:
+
+	void processEvents();
+	void processKeys(sf::Event t_event);
+	void update(sf::Time t_deltaTime);
+	void render();
+	
+	void setupFontAndText();
+	void setupSprite();
+
+	sf::RenderWindow m_window;
+	sf::Font m_ArialBlackfont;
+	sf::Text m_welcomeMessage;
+	sf::Texture m_logoTexture;
+	sf::Sprite m_logoSprite;
+	bool m_exitGame;
+
+};
+
+#endif
+
